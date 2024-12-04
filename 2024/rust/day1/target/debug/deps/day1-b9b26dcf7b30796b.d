455828b4206530aaa0da3d4d5b9623f1a4713dfe
The-Phoenics <kar9205an@gmail.com>
2024-12-04 09:01:33 +0530

    add .gitignore for rust

--- 2024/rust/day1/target/debug/deps/day1-b9b26dcf7b30796b.d
+++
@@ -1,5 +0,0 @@
-/home/phoenix/dev/dsa-aoc/2024/rust/day1/target/debug/deps/libday1-b9b26dcf7b30796b.rmeta: src/main.rs
-
-/home/phoenix/dev/dsa-aoc/2024/rust/day1/target/debug/deps/day1-b9b26dcf7b30796b.d: src/main.rs
-
-src/main.rs:
--- 2024/rust/day2/target/debug/deps/day2-67e11e62297e9952.d
+++
@@ -1,5 +0,0 @@
-/home/phoenix/dev/dsa-aoc/2024/rust/day2/target/debug/deps/libday2-67e11e62297e9952.rmeta: src/main.rs
-
-/home/phoenix/dev/dsa-aoc/2024/rust/day2/target/debug/deps/day2-67e11e62297e9952.d: src/main.rs
-
-src/main.rs:
